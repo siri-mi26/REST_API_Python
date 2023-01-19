922ca6d801546243ffc698c4773774e508db9244
siri-mi26 <sirimissula@gmail.com>
2023-01-19 12:50:43 +0800

    created index and drink routes

---
+++ api/application.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return ("hello")
+
+
+@app.route('/drinks')
+def get_drinks():
+    return {"drinks":"drink data"}
